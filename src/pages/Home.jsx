62c9c90f92a5346647fe57a76b676273d6ef6bb0
Javi Guerra <javier.guerra@npower.org>
2024-07-07 15:22:07 -0700

    feat(home page): home page UI is complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,29 @@ const Home = () => {
   return (
     <main className="home-container">
       <section className="home-hero home-subsection">
-        <h2 className="text-4xl">Hi, I am {fullName}</h2>
-        <p className="text-lg">{personalQuote}</p>
-        <NavLink to="/contact">Contact Me</NavLink>
+        <h2 className="text-4xl mb-2">Hi, I am {fullName}</h2>
+        <p className="subsection-paragraph">{personalQuote}</p>
+        <NavLink to="/contact">
+        Contact Me
+        </NavLink>
+        {/* Replace the src with your own placeholder and change the alt to match the description */}
         <img className="hero-background" src={PlaceholderHero} alt="placeholder hero image" />
       </section>
       <section className="overview home-subsection">
-        <h3>Introduction</h3>
-        <h2>Overview.</h2>
-        <p>{professionalSummary}</p>
-        <NavLink to="/about">Learn More About Me</NavLink>
+        <h3 className="subsection-intro-header">Introduction</h3>
+        <h2 className="subsection-big-header">Overview.</h2>
+        <p className="subsection-paragraph">{professionalSummary}</p>
+        <NavLink to="/about">
+        Learn More About Me
+        </NavLink>
       </section>
-      <section className="home-subsection">
-        <h3>My Work</h3>
-        <h2>Projects.</h2>
-        <p>I have worked on multiple project, the projects I have worked on demonstrate my skills and experience through real-life examples of my work. Each project is briefly deescribed and includes links to code repositories that showcase my ability to tackle challenging problems. If you are interested in my work click on the link below to go to the project page.</p>
-        <NavLink to="/projects">View Projects</NavLink>
+      <section className="projects home-subsection">
+        <h3 className="subsection-intro-header">My Works</h3>
+        <h2 className="subsection-big-header">Projects.</h2>
+        <p className="subsection-paragraph">I have worked on multiple project, the projects I have worked on demonstrate my skills and experience through real-life examples of my work. Each project is briefly deescribed and includes links to code repositories that showcase my ability to tackle challenging problems. If you are interested in my work click on the link below to go to the project page.</p>
+        <NavLink to="/projects">
+        View Projects
+        </NavLink>
       </section>
     </main>
   )
